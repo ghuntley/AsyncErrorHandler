d77457e22041c7e7e205dc735e029723ae69ff57
Brendan Forster <me@brendanforster.com>
2012-07-21 22:47:42 +1000

    updated sample and it works woo!

--- Sample/App8/MainPage.xaml.cs
+++ Sample/App8/MainPage.xaml.cs
@@ -18,7 +18,15 @@ namespace App8
 
         private async void ButtonClick(object sender, RoutedEventArgs e)
         {
+            // interesting side note:
+            // the exception raised below is piped out to our handler
+            // but the method itself has an async/await pair
+            // and it raises the unhandled exception
             await MethodWithThrow();
+
+            // this code does not suffer that issue
+            // and allows you to drop the async keyword from the method signature
+            //MethodWithThrow()
         }
 
         public async Task MethodWithThrow()
